ef0818a32cf5eb0a66dfe9787cb28267188ef379
NAOT0 <NAOT0@example.com>
2026-07-01 17:24:05 +0900

    flowerAIの記事追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,18 @@ import react from "@astrojs/react";
 import icon from "astro-icon";
 
 import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon(), mdx()],
+  integrations: [
+    react(),
+    icon(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  ],
   site: "https://NAOT0.github.io",
   base: "/Portfolio_site",
   trailingSlash: "always",
